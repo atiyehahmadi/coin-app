e196c13e6b735faab3e842625b496c28149e9368
atiyehahmadi <atiyeh.ahmadi.a@gmail.com>
2026-02-02 11:43:02 +0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+import React, { Suspense } from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
 
-export default function Home() {
+const Page = async () => {
   return (
-    <div >
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
 
-    </div>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
   );
-}
+};
+
+export default Page;
